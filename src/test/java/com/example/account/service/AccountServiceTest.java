f444a610ef724e7625ed5b8d8834d4641392ade3
selee96 <pictur96@naver.com>
2024-07-14 23:06:18 +0900

    controller, service test

--- src/test/java/com/example/account/service/AccountServiceTest.java
+++ src/test/java/com/example/account/service/AccountServiceTest.java
@@ -6,15 +6,19 @@ import com.example.account.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
@@ -34,13 +38,31 @@ class AccountServiceTest {
                         .accountStatus(AccountStatus.UNREGISTERED)
                         .accountNumber("12345")
                         .build()));
+        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
 
         // when
         Account account = accountService.getAccount(22344L);
 
         // then
+        verify(accountRepository, times(1)).findById(captor.capture());
+        verify(accountRepository, times(0)).save(any());
+        assertEquals(22344L, captor.getValue());
+        assertNotEquals(223443L, captor.getValue());
+        assertTrue(22344L == captor.getValue());
         assertEquals("12345", account.getAccountNumber());
         assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+    }
+
+    @Test
+    @DisplayName("계좌 조회 실패 - 음수로 조회")
+    void testFailedToSearchAccount() {
+        // given
+        // when
+        RuntimeException runtimeException = assertThrows(RuntimeException.class,
+                () -> accountService.getAccount(-10L));
+
+        // then
+        assertEquals("Minus", runtimeException.getMessage());
     }
 
     @Test
