f37143b367899a3b7518f3c50b5b53a94f408991
selee96 <pictur96@naver.com>
2024-07-27 08:46:09 +0900

    잔액사용 확인 api controller, service

--- src/main/java/com/example/account/service/TransactionService.java
+++ src/main/java/com/example/account/service/TransactionService.java
@@ -127,4 +127,9 @@ public class TransactionService {
 
         saveAndGetTransaction(CANCEL, amount, account, F);
     }
+
+    public TransactionDto queryTransaction(String transactionId) {
+        return TransactionDto.fromEntity(transactionRepository.findByTransactionId(transactionId)
+                .orElseThrow(() -> new AccountException(ErrorCode.TRANSACTION_NOT_FOUND)));
+    }
 }
